7fcdab9b6e40e352c28bfb408fda973eca821bbb
Lincon <linconmartins18@gmail.com>
2022-05-25 15:23:30 -0300

    String

--- CityBank-Heranca-CONTA/src/br/com/City/Banco/teste/TesteString.java
+++ CityBank-Heranca-CONTA/src/br/com/City/Banco/teste/TesteString.java
@@ -3,13 +3,39 @@ package br.com.City.Banco.teste;
 public class TesteString {
 
 	public static void main(String[] args) {
-
+		String vazio = " Alura ";
+		String outroVazio = vazio.trim();
+		
+		System.out.println(vazio.contains("Alu"));
+		System.out.println(outroVazio);
+		
+		
 		String name = "Alura"; // Object literal
-//		String outra = name.replace("A", "a");
-		System.out.println(name);
+//		String outro = new String("Alura";)
+		
+		System.out.println(name.length());
+		for(int i = 0; i < name.length(); i++) {
+			System.out.println(name.charAt(i));
+		}
+		
+//		String sub = name.substring(1);
+//		System.out.println(sub);
+		
+//		int pos = name.indexOf("ur");
+//		System.out.println(pos);
 
-		String outra = name.toLowerCase();
-		System.out.println(outra);
+//		char c = name.charAt(2);
+//
+//		System.out.println(c);
+//		char c = 'A';
+//		char d = 'a';
+//
+//		String outra = name.replace(c, d);
+
+//		String outra = name.toUpperCase();
+//
+//		System.out.println(name);
+//		System.out.println(outra);
 
 	}
 
