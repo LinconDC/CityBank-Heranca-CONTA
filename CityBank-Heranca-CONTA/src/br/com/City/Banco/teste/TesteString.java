7e04b897e5b8e9238d00e42d93684a3ac9dc5a12
Lincon <linconmartins18@gmail.com>
2022-05-25 15:04:47 -0300

    Object and String

---
+++ CityBank-Heranca-CONTA/src/br/com/City/Banco/teste/TesteString.java
@@ -0,0 +1,16 @@
+package br.com.City.Banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String name = "Alura"; // Object literal
+//		String outra = name.replace("A", "a");
+		System.out.println(name);
+
+		String outra = name.toLowerCase();
+		System.out.println(outra);
+
+	}
+
+}
